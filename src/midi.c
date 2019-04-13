7e475ebd7869016f8f3939da4eb782a02963c419
Mycal <lowerpower@users.noreply.github.com>
2019-04-13 14:19:54 -0700

    add the midi files for the project framework

---
+++ src/midi.c
@@ -0,0 +1,234 @@
+/*
+ * Midi In
+ *
+ * Copyright (c) 2019 mycal 
+ *
+ */
+
+
+#include "mytypes.h"
+#include "config.h"
+#include "debug.h"
+#include "midi.h"
+
+#include <termios.h> 
+
+volatile int             go = 0;
+MIDI                    *global_midi;
+U8				        global_flag = 0;
+
+//typedef unsigned char U8;
+//typedef unsigned short U16;
+//typedef unsigned int  U32;
+
+
+//
+// Unix handler if you need to handle CTRL-C or other signals
+//
+void
+termination_handler(int signum)
+{
+    if (global_midi->verbose) printf("term handler for signal %d\n", signum);
+
+    sprintf(global_midi->last_msg, "term handler for signal %d\n", signum);
+    //write_statistics(global_chat_ptr);
+    go = 0;
+
+    if ((SIGFPE == signum) || (SIGSEGV == signum) || (11 == signum))
+    {
+        yprintf("Server Channel Terminated from Signal %d\n", signum);
+        if (global_flag&GF_DAEMON) syslog(LOG_ERR, "Server Channel Terminated from Signal 11\n");
+
+#if defined(BACKTRACE_SYMBOLS)
+        {
+            // addr2line?                
+            void* callstack[128];
+            int i, frames = backtrace(callstack, 128);
+            char** strs = backtrace_symbols(callstack, frames);
+            yprintf("backtrace:\n");
+            for (i = 0; i < frames; ++i)
+            {
+                yprintf("T->%s\n", strs[i]);
+                if (global_flag&GF_DAEMON)  syslog(LOG_ERR, "T->%s\n", strs[i]);
+            }
+            free(strs);
+            fflush(stdout);
+        }
+#endif
+        exit(11);
+    }
+}
+
+
+
+//
+// Banner for Software
+//
+void
+startup_banner()
+{
+    //------------------------------------------------------------------
+    // Print Banner
+    //------------------------------------------------------------------
+    printf("server_channel_handler built " __DATE__ " at " __TIME__ "\n");
+    printf("   Version " VERSION " -  (c)2019 remot3.it, Inc. All Rights Reserved\n");
+    fflush(stdout);
+}
+
+//
+// Usage for Software
+//
+void usage(int argc, char **argv)
+{
+    startup_banner();
+
+    printf("usage: %s [-h] [-v(erbose)] [-d][pid file] [-f config_file]  \n", argv[0]);
+    printf("\t -h this output.\n");
+    printf("\t -v console debug output.\n");
+    printf("\t -d runs the program as a daemon pid file must be specified.\n");
+    printf("\t -f specify a config file.\n");
+    exit(2);
+}
+
+int main(int argc, char **argv)
+{
+    int				c, i;
+    int             file_ret = 0;
+    U32				timestamp = second_count();
+    MIDI            midi_static;
+    MIDI            *midi=&midi_static;
+    char            buf2[10];
+    int             count = 0;
+    struct termios  options;
+
+
+
+    // Set defaults
+    memset(midi)
+
+
+    //
+    // Banner
+    startup_banner();
+
+
+
+    //------------------------------------------------------------------
+    // Initialize error handling and signals
+    //------------------------------------------------------------------
+    //	SetConsoleCtrlHandle(termination_handler,TRUE);
+
+    if (signal(SIGINT, termination_handler) == SIG_IGN)
+        signal(SIGINT, SIG_IGN);
+    if (signal(SIGTERM, termination_handler) == SIG_IGN)
+        signal(SIGTERM, SIG_IGN);
+    if (signal(SIGILL, termination_handler) == SIG_IGN)
+        signal(SIGILL, SIG_IGN);
+    if (signal(SIGFPE, termination_handler) == SIG_IGN)
+        signal(SIGFPE, SIG_IGN);
+    //if (signal (SIGSEGV , termination_handler) == SIG_IGN)
+    //	signal (SIGSEGV , SIG_IGN);
+#if defined(LINUX) || defined(MACOSX) || defined(IOS)
+    if (signal(SIGXCPU, termination_handler) == SIG_IGN)
+        signal(SIGXCPU, SIG_IGN);
+    if (signal(SIGXFSZ, termination_handler) == SIG_IGN)
+        signal(SIGXFSZ, SIG_IGN);
+#endif
+
+
+    //
+    // Load Command Line Args, they overrie config file, reset optarg=1 to rescan
+    //
+    optind = 1;
+    while ((c = getopt(argc, argv, "f:u:l:d:vh")) != EOF)
+    {
+        switch (c)
+        {
+        case 0:
+            break;
+        case 'l':
+            //log level
+            sc.log_level = atoi(optarg);
+            break;
+        case 'u':
+            //udp Port
+            sc.udp_listen_port = atoi(optarg);
+            break;
+        case 'd':
+            // Startup as daemon with pid file
+            printf("Starting up as daemon\n");
+            strncpy(sc.pidfile, optarg, MAX_PATH - 1);
+            global_flag = global_flag | GF_DAEMON;
+            break;
+        case 'v':
+        case 'f':
+            // Do nothing, did it above
+            break;
+        case 'h':
+            usage(argc, argv);
+            break;
+        default:
+            usage(argc, argv);
+            break;
+        }
+    }
+    argc -= optind;
+    argv += optind;
+
+    int sfd = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY);
+    if (sfd == -1) {
+        printf("Error no is : %d\n", errno);
+        printf("Error description is : %s\n", strerror(errno));
+        return(-1);
+    }
+
+    //raw mode. no echo, with 8n1 at 38400 baud 
+    tcgetattr(sfd, &options);
+    cfsetspeed(&options, B38400);
+    options.c_cflag &= ~CSTOPB;
+    options.c_cflag |= CLOCAL;
+    options.c_cflag |= CREAD;
+    cfmakeraw(&options);
+    tcsetattr(sfd, TCSANOW, &options);
+
+
+#if !defined(WIN32)
+    //
+    // Should Daemonize here if set
+    //
+    if (global_flag&GF_DAEMON)
+    {
+        if (sc.verbose) printf("Calling Daemonize\n");
+
+        // Setup logging
+        openlog("server_channel", LOG_PID | LOG_CONS, LOG_USER);
+        syslog(LOG_INFO, "Server Channel built "__DATE__ " at " __TIME__ "\n");
+        syslog(LOG_INFO, "   Version " VERSION " -  (c)2019 remot3.it, Inc. All Rights Reserved\n");
+        syslog(LOG_INFO, "Starting up as daemon\n");
+        syslog(LOG_INFO, "Bound to UDP %d.%d.%d.%d:%d on socket %d\n", sc.Bind_IP.ipb1, sc.Bind_IP.ipb2, sc.Bind_IP.ipb3, sc.Bind_IP.ipb4, sc.udp_listen_port, sc.udp_listen_soc);
+
+
+        // Daemonize this
+        daemonize(sc.pidfile, sc.run_as_user, 0, 0, 0, 0, 0);
+    }
+#endif
+
+
+
+    // Read Forever, or until killed
+    go = 1;
+    while (go)
+    {
+        count = read(sfd, buf2, 1);
+        buf2[1] = 0;
+        printf("count= %d  --> %x\n", count, buf2[0]);
+    }
+	
+
+printf("shutdown\n");
+
+	return 0;
+}
+
+
+
---
+++ src/midi.h
@@ -0,0 +1,61 @@
+#ifndef __MIDI_H__
+#define __MIDI_H__
+//---------------------------------------------------------------------------
+// midi.h							-
+//---------------------------------------------------------------------------
+// Version                                                                  -
+//		0.1 Original Version April 3, 2019     							-
+//																			-
+// (c)2019 Mycal.net					-
+//---------------------------------------------------------------------------
+
+
+
+
+//
+// GF flags, global flags
+//
+#define	GF_GO			0x01				/* go */
+#define GF_DAEMON		0x02				/* we are a daemon */
+#define GF_QUIET		0x08				/* no output */
+#define GF_CMD_LINE		0x10				/*  */
+#define GF_CMD_PROC		0x20				/* Turn on command line processor */
+#define GF_BANG_STATUS	0x40				/* turn on stat output */
+
+extern U8              global_flag;
+
+
+// Custom File config for each product here, this one is for server channel config
+typedef struct midi_config_
+{
+    // Target Relay Driver
+    U16			des_port;
+    IPADDR		dest_IP;
+    SOCKET		soc;
+    //
+    int			verbose;
+    int			log_level;
+    int			auto_reload;
+    //
+
+    // current relay mask
+    char        bitmask[8];
+
+    // relay map
+    int         map[128][16];
+
+
+    char		config_file[MAX_PATH];
+    char		map_file[MAX_PATH];
+    char        run_as_user[MAX_PATH];
+    char        chroot[MAX_PATH];
+
+
+    // Stats
+
+
+    // Pidfile for daemon
+    char		pidfile[MAX_PATH];
+}MIDI;
+
+#endif
